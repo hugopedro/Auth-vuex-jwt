1e12a67a5304f28782fae2829133b9255a4c46d7
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-09-18 11:58:01 -0300

    Usando autenticacao Central

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,14 +1,25 @@
 import axios from 'axios';
 
 const API_URL = `${process.env.VUE_APP_API_URL}/api/auth/`;
+const API_CENTRAL = `${process.env.VUE_APP_CENTRAL_URL}/`;
 
 class AuthService {
+
+  handleCredentials(user) {
+    
+    const planCredentials = `${user.username}:${user.password}`;
+    const encodedCredentials = btoa(planCredentials);
+    const credentials = `Basic ${encodedCredentials}`;
+
+    return credentials;
+  }
+
   login(user) {
+    const credentials = this.handleCredentials(user);
+    const config = {headers:{Authorization:credentials}}
+    
     return axios
-      .post(API_URL + 'signin', {
-        username: user.username,
-        password: user.password
-      })
+      .post(API_CENTRAL + 'login', null, config)
       .then(response => {
         if (response.data.accessToken) {
           localStorage.setItem('user', JSON.stringify(response.data));
